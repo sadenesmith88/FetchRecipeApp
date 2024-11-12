7fe9acea28c0f83d6eaf6447950b33c9e255c399
Sade Nesmith <sadework21@gmail.com>
2024-11-12 14:57:27 -0500

    Initial Commit

---
+++ FetchTakeHomeProj/FetchTakeHomeProjApp.swift
@@ -0,0 +1,17 @@
+//
+//  FetchTakeHomeProjApp.swift
+//  FetchTakeHomeProj
+//
+//  Created by sade on 11/12/24.
+//
+
+import SwiftUI
+
+@main
+struct FetchTakeHomeProjApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
